c2227d621d498cdce28383fad516c8f0fbca9391
elinaasratyan <asel24mr@student.ju.se>
2025-02-28 02:35:51 +0100

    added animation for the social links

--- js/main.js
+++ js/main.js
@@ -21,3 +21,20 @@ for (let i = 0; i < navigationElements.length; i++) {
     });
   }
 }
+
+for (let i = 0; i < socialElements.length; i++) {
+  // Get all icons inside the current navigation menu
+  const socialIcons = socialElements[i].getElementsByTagName("i");
+
+  // Scale when the mouse is over
+  for (let j = 0; j < socialIcons.length; j++) {
+    socialIcons[j].addEventListener("mouseover", function () {
+      this.style.transform = "scale(1.2)";
+    });
+
+    // Unscale back to the initial size when the mouse leaves
+   socialIcons[j].addEventListener("mouseleave", function () {
+      this.style.transform = "scale(1)";
+    });
+  }
+}
