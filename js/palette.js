ab28f21b2ff2b73936ac24a595ad1d324c10074a
elinaasratyan <asel24mr@student.ju.se>
2025-03-10 11:55:58 +0100

    added palette.js for plus button
    Also changed palette.html classes and adjusted makup.html so the defult price setting is 300 and not 150 to see all the products on the page

---
+++ js/palette.js
@@ -0,0 +1,17 @@
+    // Select the toggle button and the ingredients content
+    const plusButton = document.querySelector(".plus-button");
+    const ingredientsContent = document.querySelector(".ingredients-content");
+  
+    
+    plusButton.addEventListener("click", function () {
+      // Toggle the "hidden" class
+      // This was taken from previous work on profile assignment and modified to fit the current assignment: https://github.com/front-end-fundamentals-2025/fef-portfolio-elinaasratyan/blob/main/js/main.js
+      ingredientsContent.classList.toggle("hidden");
+      
+      // Change the button text from "+" to "-" or vice versa
+      if (ingredientsContent.classList.contains("hidden")) {
+        plusButton.textContent = "+";
+      } else {
+        plusButton.textContent = "-";
+      }
+    });
